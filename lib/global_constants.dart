7f1691fa1d1fc765fdd90171a20efc2bc541a015
Manfred Kern <manfred.kern@gmail.com>
2024-02-08 17:43:06 +0100

    Enum for CsvInputfield 2

--- lib/global_constants.dart
+++ lib/global_constants.dart
@@ -16,74 +16,34 @@ const List<String> normalSpeed = ['15 - 18 km/h'];
 const int altitudeThreshold = 300; // up to 300Hm normal, above mountainous
 const List<String> mountainous = ["hügelig", "bergig"];
 
-// TODO: replace CSV class with Enum
-
-enum CsvInputFields {
-  titleField(3, 'Titel'),
-  shortDescriptionField(4, 'Kurzbeschreibung'),
-  descriptionField(5, 'Beschreibung'),
-  startDateField(11, 'Beginndatum'),
-  startTimeField(12, 'Beginnzeit'),
-  endDateField(13, 'Endedatum'),
-  tourGuideField(26, 'Tourguide'),
-  lengthField(32, 'Länge'),
-  speedField(33, 'Geschwindigkeit'),
-  altitudeDescField(35, 'Höhenbewertung'),
-  speedDescField(37, 'Geschwindigkeitsbereich'),
-  difficultyField(39, 'Schwierigkeitsgrad'),
-  streetField(40, 'Strasse'),
-  cityField(41, 'Stadt'),
-  nextStreetField(67, 'Zwischenstation 1 - Strasse'),
-  nextCityField(68, 'Zwischenstation 1 - Stadt');
+// Input fields to process of CSV file
+enum CsvInputField {
+  title(3, 'Titel'),
+  shortDescription(4, 'Kurzbeschreibung'),
+  description(5, 'Beschreibung'),
+  startDate(11, 'Beginndatum'),
+  startTime(12, 'Beginnzeit'),
+  endDate(13, 'Endedatum'),
+  tourGuide(26, 'Tourguide'),
+  length(32, 'Länge'),
+  speed(33, 'Geschwindigkeit'),
+  altitudeDesc(34, 'Höhenbewertung'),
+  altitude(35, 'Höhenmeter'),
+  speedDesc(37, 'Geschwindigkeitsbereich'),
+  difficulty(39, 'Schwierigkeitsgrad'),
+  street(40, 'Strasse'),
+  city(41, 'Stadt'),
+  nextStreet(67, 'Zwischenstation 1 - Strasse'),
+  nextCity(68, 'Zwischenstation 1 - Stadt');
 
   final int number;
   final String text;
-  const CsvInputFields(this.number, this.text);
+  const CsvInputField(this.number, this.text);
 }
 
 /// All related constants for parsing csv file
 class Csv {
   static const fieldDelimiter = ';';
-
-  // relevant Fields of the input csv file
-  static const titleField = 3; // Titel
-  static const shortDescriptionField = 4; // Kurzbeschreibung
-  static const descriptionField = 5; // Beschreibung
-  static const startDateField = 11; // Beginndatum
-  static const startTimeField = 12; // Zeit
-  static const endDateField = 13; // Endedatum
-  static const tourGuideField = 26; // Organisator
-  static const lengthField = 32; // Länge
-  static const speedField = 33; // Geschwindigkeit
-  static const altitudeDescField = 34; // Höhenbewertung
-  static const altitudeField = 35; // Höhenmeter
-  static const speedDescField = 37; // Geschwindigkeitsbereich
-  static const difficultyField = 39; // Schwierigkeitsgrad
-  static const streetField = 40; // Strasse
-  static const cityField = 41; // Stadt
-  static const nextStreetField = 67; // Zwischenstation 1 - Strasse
-  static const nextCityField = 68; // Zwischenstation 1 - Stadt
-
-  // Process relevant input fields
-  static const List<(int, String)> csvInputDescription = [
-    (titleField, 'Titel'),
-    (shortDescriptionField, 'Kurzbeschreibung'),
-    (descriptionField, 'Beschreibung'),
-    (startDateField, 'Beginndatum'),
-    (startTimeField, 'Beginnzeit'),
-    (endDateField, 'Endedatum'),
-    (tourGuideField, 'Tourguide'),
-    (lengthField, 'Länge'),
-    (speedField, 'Geschwindigkeit'),
-    (altitudeDescField, 'Höhenbewertung'),
-    (altitudeField, 'Höhenmeter'),
-    (speedDescField, 'Geschwindigkeitsbereich'),
-    (difficultyField, 'Schwierigkeitsgrad'),
-    (cityField, 'Stadt'),
-    (streetField, 'Strasse'),
-    (nextStreetField, 'Zwischenstation 1 - Strasse'),
-    (nextCityField, 'Zwischenstation 1 - Stadt'),
-  ];
 
   // additional fields calculated from input fields in
   // tour_record._postProcessing()   // look also for enum AdditionalFields
